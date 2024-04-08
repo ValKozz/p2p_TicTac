2288e4d1085541dde1f26dbc033b37d1d6a3eb9c
ValKozz <v.kozarev98@gmail.com>
2024-04-08 17:39:00 +0300

    Setup basic p2p peer to modify and use. Credit: https://www.linkedin.com/pulse/implementing-peer-to-peer-data-exchange-inpython-luis-soares-m-sc-/

--- src/peer.py
+++ src/peer.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 PORT = 6969
 
@@ -21,3 +22,20 @@ class Peer:
         self.socket.bind((self.host, self.port))
         self.socket.listen(10)
         print(f'Listening for connections on {self.host}:{self.port}')
+
+        while True:
+            connection, address = self.socker.accept()
+            self.connections.append(connection)
+            print(f'Accepted connection form {address}')
+
+
+    def send_data(self, data):
+        for connection in self.connections:
+            try:
+                connection.sendall(data.emcode())
+            except socket.error as e:
+                print(f'Failed to send data. Error: {e}')
+
+    def start(self):
+        listend_thread = threading.Thread(target=self.listen)
+        listend_thread.start()
